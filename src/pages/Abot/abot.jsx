9f9b6de9550d2e21fcbd5a38d088f94292280c0a
Guilherme <gui.goncalves.joi@gmail.com>
2025-09-03 22:08:02 -0300

    feat/doni

---
+++ src/pages/Abot/abot.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Abot(){
+    return(
+        <>
+        flamengo
+        <Link to={'/Rick'}>
+        <button>Navegar paara abot</button>
+        </Link>
+        </>
+    )
+    
+}
